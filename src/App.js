cf78a07ec1f50b631f9286726505d9d37558b1b8
JCanudas23 <joaquin.canudas@gmail.com>
2023-09-20 18:08:53 -0300

    corrigiendo errores

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import NavBar from "./components/NavBar/NavBar";
-import Banner from "./components/Banner/Banner"
+/* import Banner from "./components/Banner/Banner" */
 import ItemListContainer from './components/ItemListContainer/ItemListContainer';
 import ItemCount from "./components/ItemCount/ItemCount"
 import ItemDetailContainer from "./components/ItemDetailContainer/ItemDetailContainer"
@@ -10,23 +10,16 @@ import {YEAR} from "./config/constants.js"
 
 function App() {
     return (
-      /* <div className="App"> */
+
         <BrowserRouter>
             <NavBar />
-            <Banner />
             <Routes>
               <Route path="/" element = {<ItemDetailContainer />} />
               <Route path="/category/:categoryId" element = {<ItemDetailContainer />} />
               <Route path="/item/:itemId" element = {<ItemDetailContainer />} />
               <Route path="*"element = {<h1>404 NOT FOUND</h1>} /> 
             </Routes>
-            <ItemListContainer greeting = {"Bienvenidos"} />
-            <ItemCount initial={1} stock={10} onAdd={(quantity) => console.log("Cantidad agregada ", quantity)}/>
-            <footer>
-              Creada por Joaquin Canudas Ribo {YEAR}
-            </footer>
-        </BrowserRouter>  
-      /* </div>   */    
+        </BrowserRouter>     
     );
 }
 
